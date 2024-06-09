32fe31e5c9112d1b0d8ec1c378b53bab71fe64c7
Daniel Ursache Dogariu <contact@danniel.net>
2024-06-09 13:36:51 +0300

    Append instead of overwriting sheets

--- main.py
+++ main.py
@@ -44,6 +44,19 @@ with pd.ExcelWriter(result_doc) as writer:
         for sheet_num in range(0, 4):
             sheet_name = pd.ExcelFile(document).sheet_names[sheet_num]
             df = pd.read_excel(document, sheet_name)
-            df.to_excel(writer, sheet_name=sheet_index_names[sheet_num][1], index=False)
+            dest_sheet_name = sheet_index_names[sheet_num][1]
+
+            # Try to append data to an existing sheet
+            try:
+                df.to_excel(
+                    writer,
+                    sheet_name=dest_sheet_name,
+                    startrow=writer.sheets[dest_sheet_name].max_row,
+                    index=False,
+                    header=False,
+                )
+            except KeyError:
+                # Or write data to a new sheet
+                df.to_excel(writer, sheet_name=dest_sheet_name, index=False)
 
     logger.info("Finished processing all source documents.")
